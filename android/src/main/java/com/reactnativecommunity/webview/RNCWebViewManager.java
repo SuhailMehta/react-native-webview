cce574d5224539195bced1ffe999c47d596aa613
suhail.mehta <suhail.mehta@phonepe.com>
2022-08-11 19:09:04 +0530

    import added

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -20,7 +20,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
-import android.content.Intent
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
